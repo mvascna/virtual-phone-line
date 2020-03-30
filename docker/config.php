2c1c29f5abaaa9526d5efeb0906f8aaeb8d8eccd
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2020-03-29 23:01:50 -0400

    beta10

--- docker/config.php
+++ docker/config.php
@@ -1,25 +1,26 @@
 <?php
-static $mysql_hostname        = {{ getenv "MYSQL_HOST" | quote }};
-static $mysql_username        = {{ getenv "MYSQL_USER" | quote }};
-static $mysql_password        = {{ getenv "MYSQL_PASSWORD" | quote }};
-static $mysql_database        = {{ getenv "MYSQL_DATABASE" | quote }};
-static $title                 = {{ getenv "TITLE" | quote }};
-static $bmlt_root_server      = {{ getenv "BMLT_ROOT_SERVER" | quote }};
-static $google_maps_api_key   = {{ getenv "GOOGLE_MAPS_API_KEY" | quote }};
-static $twilio_account_sid    = {{ getenv "TWILIO_ACCOUNT_SID" | quote }};
-static $twilio_auth_token     = {{ getenv "TWILIO_AUTH_TOKEN" | quote }};
-static $en_US_voice           = {{ getenv "VOICE" "Polly.Kendra" | quote }};
-static $language              = {{ getenv "LANGUAGE" "en" | quote }};
-static $infinite_searching    = {{ getenv "INFINITE_SEARCHING" "true" }};
-static $jft_option            = {{ getenv "JFT_OPTION" "true" }};
-static $bmlt_auth             = {{ getenv "BMLT_AUTH" "true" }};
-static $darkModeFeatureToggle = {{ getenv "DARK_MODE" "true" }};
-static $sms_summary_page      = {{ getenv "SMS_SUMMARY_PAGE" "false" }};
-static $virtual               = {{ getenv "VIRTUAL" "true" }};
-static $grace_minutes         = {{ getenv "GRACE_MINUTES" "15" }};
-static $result_count_max      = {{ getenv "RESULT_COUNT_MAX" "5" }};
-static $sms_combine           = {{ getenv "SMS_COMBINE" "true" }};
-static $debug                 = {{ getenv "DEBUG" "false"}};
+static $mysql_hostname         = {{ getenv "MYSQL_HOST" | quote }};
+static $mysql_username         = {{ getenv "MYSQL_USER" | quote }};
+static $mysql_password         = {{ getenv "MYSQL_PASSWORD" | quote }};
+static $mysql_database         = {{ getenv "MYSQL_DATABASE" | quote }};
+static $title                  = {{ getenv "TITLE" | quote }};
+static $bmlt_root_server       = {{ getenv "BMLT_ROOT_SERVER" | quote }};
+static $google_maps_api_key    = {{ getenv "GOOGLE_MAPS_API_KEY" | quote }};
+static $twilio_account_sid     = {{ getenv "TWILIO_ACCOUNT_SID" | quote }};
+static $twilio_auth_token      = {{ getenv "TWILIO_AUTH_TOKEN" | quote }};
+static $en_US_voice            = {{ getenv "VOICE" "Polly.Kendra" | quote }};
+static $language               = {{ getenv "LANGUAGE" "en" | quote }};
+static $infinite_searching     = {{ getenv "INFINITE_SEARCHING" "false" }};
+static $jft_option             = {{ getenv "JFT_OPTION" "true" }};
+static $bmlt_auth              = {{ getenv "BMLT_AUTH" "true" }};
+static $darkModeFeatureToggle  = {{ getenv "DARK_MODE" "true" }};
+static $sms_summary_page       = {{ getenv "SMS_SUMMARY_PAGE" "false" }};
+static $virtual                = {{ getenv "VIRTUAL" "true" }};
+static $grace_minutes          = {{ getenv "GRACE_MINUTES" "15" }};
+static $result_count_max       = {{ getenv "RESULT_COUNT_MAX" "5" }};
+static $sms_combine            = {{ getenv "SMS_COMBINE" "true" }};
+static $debug                  = {{ getenv "DEBUG" "false"}};
+static $suppress_voice_results = {{ getenv "SUPPRESS_VOICE_RESULTS" "true"}};
 {{ if getenv "DIGIT_MAP_SEARCH_TYPE" }}
 static $digit_map_search_type = {{ getenv "DIGIT_MAP_SEARCH_TYPE" }};
 {{ end }}
